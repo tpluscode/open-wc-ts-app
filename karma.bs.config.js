6da5e9a99fcd9f6dcf967a4609c8aed045005373
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2019-08-14 07:32:16 +0000

    Initial commit

---
+++ karma.bs.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const merge = require('webpack-merge')
+const bsSettings = require('@open-wc/testing-karma-bs/bs-settings.js')
+const createBaseConfig = require('./karma.conf.js')
+
+module.exports = config => {
+  config.set(
+    merge(bsSettings(config), createBaseConfig(config), {
+      browserStack: {
+        project: 'your-name',
+      },
+    }),
+  )
+
+  // eslint-disable-next-line no-param-reassign
+  config.concurrency = 1
+
+  return config
+}
