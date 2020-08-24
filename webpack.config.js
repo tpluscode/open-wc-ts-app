7c837a8f1b88cae3f0e9ff1ab3e3891368937919
tpluscode <tpluscode@users.noreply.github.com>
2020-08-24 08:36:24 +0200

    build: merge import changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const { createDefaultConfig } = require('@open-wc/building-webpack')
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
